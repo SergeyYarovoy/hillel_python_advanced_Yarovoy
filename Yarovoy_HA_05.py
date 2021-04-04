9da62b0ab16706019f099f70d62b5b3b31d27da6
serhiiyarovyi <becoss3@gmail.com>
2021-04-04 11:58:08 +0300

    Yarovoy HA04 + HA05

---
+++ Yarovoy_HA_05.py
@@ -0,0 +1,25 @@
+class BadClass:
+
+   def __init__(self, val=0):
+       self.val = val
+       self.hash_val = val
+
+   def __hash__(self):
+       return hash(self.hash_val)
+
+   def __eq__(self, other):
+       return self.__class__ == other.__class__ and self.val == other.val
+
+
+bad = BadClass()
+
+even_worse = { bad }
+print(bad.val)
+assert(bad in even_worse)
+bad.val += 1
+assert(bad in even_worse)
+print('yeah, we good!')
+
+still_bad = BadClass(val=bad.val)
+assert(still_bad in even_worse)
+
