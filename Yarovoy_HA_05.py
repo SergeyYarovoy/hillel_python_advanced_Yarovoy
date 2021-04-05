7f8ac9ca16dd36c6d6d2de06f5b0ab375fcde33d
serhiiyarovyi <becoss3@gmail.com>
2021-04-05 19:18:56 +0300

    HA_5 fixed_

--- Yarovoy_HA_05.py
+++ Yarovoy_HA_05.py
@@ -2,7 +2,7 @@ class BadClass:
 
    def __init__(self, val=0, hash_val = 0):
        self.val = val
-       self.hash_val = hash_val
+       self.hash_val = 0
 
    def __hash__(self):
        return hash(self.hash_val)
@@ -17,6 +17,8 @@ assert(bad in even_worse)
 bad.val += 1
 assert(bad in even_worse)
 print('yeah, we good!')
-still_bad = BadClass(val=bad.val)
+still_bad = BadClass(val=bad.val, hash_val='Hmmmmm!')
+#still_bad = BadClass(val=bad.val)
 assert(still_bad in even_worse)             # Assertion
 
+sudo iwlist wlp3s0 scan | grep ESSID
